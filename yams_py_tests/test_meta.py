735c356a78f0830ffc32ec25e858fe26de3a1928
Miepee <janbidler00@protonmail.com>
2023-07-24 17:51:22 +0200

    Change one more underscore

--- yams_py_tests/test_meta.py
+++ yams_py_tests/test_meta.py
@@ -6,7 +6,7 @@ from yams_py import Patcher
 
 def test_correct_versions():
     cs_version = Patcher.get_lib_version()
-    python_version = version("yams-py")
+    python_version = version("yams_py")
 
     assert cs_version != ""
     assert cs_version == python_version
