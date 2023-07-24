da11c0fd514a4e5e271f74757c6017bba998f2da
Miepee <janbidler00@protonmail.com>
2023-07-24 17:12:18 +0200

    Fix goofup in test

--- yams_py_tests/test_meta.py
+++ yams_py_tests/test_meta.py
@@ -8,5 +8,5 @@ def test_correct_versions():
     cs_version = Patcher.get_lib_version()
     python_version = version("yams-py")
 
-    assert cs_match is not None and python_version is not None
-    assert cs_match.group(1) == python_version
+    assert cs_version != ""
+    assert cs_version == python_version
