b12b29dd772794f873c7a5b7950e6bb9d4cd2877
Miepee <janbidler00@protonmail.com>
2023-07-24 18:47:11 +0200

    Rename python wrapper from `yams_py` to `am2r_yams`

--- am2r_yams_tests/test_meta.py
+++ am2r_yams_tests/test_meta.py
@@ -1,12 +1,12 @@
 import re
 from pathlib import Path
 from importlib.metadata import version
-from yams_py import Patcher
+from am2r_yams import Patcher
 
 
 def test_correct_versions():
     cs_version = Patcher.get_lib_version()
-    python_version = version("yams_py")
+    python_version = version("am2r_yams")
 
     assert cs_version != ""
     assert cs_version == python_version
