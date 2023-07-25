fc3df19e0b7af5971ca1820a85bd6126cf07fdf6
Miepee <janbidler00@protonmail.com>
2023-07-25 22:49:30 +0200

    make wrapper manage dotnet runtime

--- am2r_yams/Patcher.py
+++ am2r_yams/Patcher.py
@@ -9,69 +9,80 @@ from pathlib import Path
 from typing import Callable
 
 # TODO: revise this to be cleaner. Following things to keep in mind here
-# 1. AFAIK the DLLs must be in path before loading the CLR. test whether that's truly the case
-# 2. the CLR should only loaded *once*. I.e importing this module multiple times shouldnt cause exceptions of trying to load it multiple times.
-# 3. Figure out how to deal with non-system dotnet installations
+# 1. AFAIK the DLLs must be in path before loading the CLR. test whether that's truly the case.
+#    Maybe we can remove from path when we're done?
+# 2. Figure out how to deal with non-system dotnet installations
 yams_path = os.fspath(Path(__file__).with_name(name="yams"))
 sys.path.append(yams_path)
-from pythonnet import load
-
-load("coreclr")
-import clr
-
-clr.AddReference("YAMS-LIB")
-from YAMS_LIB import Patcher
+from pythonnet import load, unload
 
 
-def get_lib_version() -> str:
-    return Patcher.Version
+# TODO: add docstrings for methods when not in alpha and has stableish API
+class Patcher:
+    def __enter__(self):
+        # Load dotnet runtime
+        load("coreclr")
+        import clr
 
+        clr.AddReference("YAMS-LIB")
+        from YAMS_LIB import Patcher as CS_Patcher
 
-# TODO: ADD TESTS!!!
-def patch_game(
-    input_path: Path,
-    output_path: Path,
-    patch_data: dict,
-    progress_update: Callable[[str, float], None],
-):
-    # Copy to input dir to temp dir first to do operations there
-    progress_update("Copying to temporary path...", 0)
-    tempdir = tempfile.TemporaryDirectory()
-    shutil.copytree(input_path, tempdir.name, dirs_exist_ok=True)
+    def __exit__(self, *args):
+        # Unload dotnet runtime and references
+        del Patcher
+        unload()
+        del clr
 
-    # Get data.win path. Both of these *need* to be strings, as otherwise patcher won't accept them.
-    output_data_win: str = os.fspath(
-        _prepare_environment_and_get_data_win_path(tempdir.name)
-    )
-    input_data_win: str = shutil.move(output_data_win, output_data_win + "_orig")
-    input_data_win_path = Path(input_data_win)
+    def get_lib_version() -> str:
+        return CS_Patcher.Version
 
-    # TODO: do some check on whether input is valid? on patcher side probably.
+    # TODO: ADD TESTS!!!
+    def patch_game(
+        input_path: Path,
+        output_path: Path,
+        patch_data: dict,
+        progress_update: Callable[[str, float], None],
+    ):
+        # Copy to input dir to temp dir first to do operations there
+        progress_update("Copying to temporary path...", 0)
+        tempdir = tempfile.TemporaryDirectory()
+        shutil.copytree(input_path, tempdir.name, dirs_exist_ok=True)
 
-    # Temp write patch_data into json file for yams later
-    progress_update("Creating json file...", 0.3)
-    json_file: str = os.fspath(input_data_win_path.parent.joinpath("yams-data.json"))
-    with open(json_file, "w+") as f:
-        f.write(json.dumps(patch_data))
+        # Get data.win path. Both of these *need* to be strings, as otherwise patcher won't accept them.
+        output_data_win: str = os.fspath(
+            _prepare_environment_and_get_data_win_path(tempdir.name)
+        )
+        input_data_win: str = shutil.move(output_data_win, output_data_win + "_orig")
+        input_data_win_path = Path(input_data_win)
 
-    # AM2RLauncher installations usually have a profile.xml file. For less confusion, remove it if it exists
-    profile_xml_path = Path(tempdir.name).joinpath("profile.xml")
-    if profile_xml_path.exists():
-        profile_xml_path.unlink()
+        # TODO: do some check on whether input is valid? on patcher side probably.
 
-    # TODO: this is where we'd call the patcher to do some customization options like music shuffler or samus palettes
+        # Temp write patch_data into json file for yams later
+        progress_update("Creating json file...", 0.3)
+        json_file: str = os.fspath(
+            input_data_win_path.parent.joinpath("yams-data.json")
+        )
+        with open(json_file, "w+") as f:
+            f.write(json.dumps(patch_data))
 
-    # Patch data.win
-    progress_update("Patching data file...", 0.6)
-    Patcher.Main(input_data_win, output_data_win, json_file)
+        # AM2RLauncher installations usually have a profile.xml file. For less confusion, remove it if it exists
+        profile_xml_path = Path(tempdir.name).joinpath("profile.xml")
+        if profile_xml_path.exists():
+            profile_xml_path.unlink()
 
-    # Move temp dir to output dir and get rid of it. Also delete original data.win
-    input_data_win_path.unlink()
-    progress_update("Moving to output directory...", 0.8)
-    shutil.copytree(tempdir.name, output_path, dirs_exist_ok=True)
-    shutil.rmtree(tempdir.name)
+        # TODO: this is where we'd call the patcher to do some customization options like music shuffler or samus palettes
 
-    progress_update("Exporting finished!", 1)
+        # Patch data.win
+        progress_update("Patching data file...", 0.6)
+        CS_Patcher.Main(input_data_win, output_data_win, json_file)
+
+        # Move temp dir to output dir and get rid of it. Also delete original data.win
+        input_data_win_path.unlink()
+        progress_update("Moving to output directory...", 0.8)
+        shutil.copytree(tempdir.name, output_path, dirs_exist_ok=True)
+        shutil.rmtree(tempdir.name)
+
+        progress_update("Exporting finished!", 1)
 
 
 def _prepare_environment_and_get_data_win_path(folder: str) -> Path:
--- am2r_yams_tests/test_meta.py
+++ am2r_yams_tests/test_meta.py
@@ -5,7 +5,9 @@ from am2r_yams import Patcher
 
 
 def test_correct_versions():
-    cs_version = Patcher.get_lib_version()
+    cs_version = ""
+    with Patcher() as p:
+        cs_version = p.get_lib_version()
     python_version = version("am2r_yams")
 
     assert cs_version != ""
