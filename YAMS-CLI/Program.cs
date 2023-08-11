cebe325ab1a1d95de0ce1e76d72dc03dd6914970
Miepee <janbidler00@protonmail.com>
2023-08-11 10:46:12 +0200

    Get rid of debug line in cli

--- YAMS-CLI/Program.cs
+++ YAMS-CLI/Program.cs
@@ -1,29 +1,20 @@
 ﻿using System;
 using YAMS_LIB;
 
-var debug = true;
 string am2rPath = "";
 string outputAm2rPath = "";
 string jsonPath = "";
-if (debug)
+
+if (args.Length < 3)
 {
-    am2rPath = @"/home/narr/Dokumente/am2r 1.5.5/assets/game.unx_older";
-    outputAm2rPath = @"/home/narr/Dokumente/am2r 1.5.5/output/assets/game.unx";
-    jsonPath = @"/home/narr/Dokumente/am2r 1.5.5/output/assets/yams-data.json";
+    Console.WriteLine("Insufficient arguments!");
+    Console.WriteLine("Usage: ./YAMS [path-to-original-data-file] [path-to-output-data-file] [path-to-json-file]");
+    return -1;
 }
-else
-{
-    if (args.Length < 3)
-    {
-        Console.WriteLine("Insufficient arguments!");
-        Console.WriteLine("Usage: ./YAMS [path-to-original-data-file] [path-to-output-data-file] [path-to-json-file]");
-        return -1;
-    }
-    
-    am2rPath = args[0];
-    outputAm2rPath = args[1];
-    jsonPath = args[2];
-}
+
+am2rPath = args[0];
+outputAm2rPath = args[1];
+jsonPath = args[2];
 
 Patcher.Main(am2rPath, outputAm2rPath, jsonPath);
 return 0;
