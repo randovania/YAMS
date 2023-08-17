eca1dfd73721ce2b0e95400c03b4a3b4d8fac9d1
Miepee <janbidler00@protonmail.com>
2023-08-18 01:13:34 +0200

    Add room name display as configurable setting
    Co-Authored-By: Jesright73 <79653967+Jesright73@users.noreply.github.com>

--- YAMS-LIB/Program.cs
+++ YAMS-LIB/Program.cs
@@ -2825,17 +2825,133 @@ public class Patcher
         ReplaceGMLInCode(gmData.Code.ByName("gml_Object_oItem_Other_10"), "display_itemmsg", "if (!global.skipItemFanfare) display_itemmsg");
         
         
-        // Go through every room's creation code, and set popup_text(room_name)
-        // TODO: make this an gameplay patching option
-        foreach ((var internalName, var room) in seedObject.RoomObjects)
+        // Patch to add room name display near health
+        string roomNameDSMapString = "";
+        foreach ((var roomName, var roomData) in seedObject.RoomObjects)
+            roomNameDSMapString += $"ds_map_add(roomNames, \"{roomName}\", \"{roomData.DisplayName}\");\n";
+
+        var roomNameHudCC = new UndertaleCode() { Name = gmData.Strings.MakeString("gml_Object_oRoomNameHUD_Create_0") };
+        SubstituteGMLCode(roomNameHudCC, $"""
+        rnh_surface = surface_create((320 + oControl.widescreen_space), 240)
+        fadeout = 0
+        roomtime = 0
+        textToDisplay = ""
+        image_alpha = 0
+        offsetY = 0
+        displayMode = {(int)seedObject.Cosmetics.RoomNameHud}
+        textBGColor = 0
+        textColor0 = c_white
+        textColor1 = c_silver
+        roomNames = ds_map_create()
+        {roomNameDSMapString}
+        """);
+        var roomNameHudDestroy = new UndertaleCode() { Name = gmData.Strings.MakeString("gml_Object_oRoomNameHUD_Destroy_0") };
+        SubstituteGMLCode(roomNameHudDestroy, "surface_free(rnh_surface); ds_map_destroy(roomNames)");
+        var roomNameHudDrawGui = new UndertaleCode() { Name = gmData.Strings.MakeString("gml_Object_oRoomNameHUD_Draw_64") };
+        SubstituteGMLCode(roomNameHudDrawGui, """
+        var d;
+        d = application_get_position()
+        if (room != rm_transition && instance_exists(oCharacter) && global.ingame && displayMode != 0 && global.opshowhud)
+            draw_surface_ext(rnh_surface, (oControl.displayx - d[0]), (oControl.displayy - d[1]), oControl.display_scale, oControl.display_scale, 0, -1, 1)
+        """);
+        var roomNameHudRoomStart = new UndertaleCode() {Name = gmData.Strings.MakeString("gml_Object_oRoomNameHUD_Other_4")};
+        SubstituteGMLCode(roomNameHudRoomStart, """
+        var newRoomName = ds_map_find_value(roomNames, room_get_name(room))
+        if (global.ingame && textToDisplay != newRoomName)
         {
-            var roomName = room.DisplayName;
-            if (String.IsNullOrWhiteSpace(roomName)) continue;
-                
-            var gmRoom = gmData.Rooms.ByName(internalName);
-            AppendGMLInCode(gmRoom.CreationCodeId, $"popup_text(\"{roomName}\")");
+            fadeout = 0
+            roomtime = 0
+            image_alpha = 0
+            textToDisplay = newRoomName
         }
-            
+        """);
+        var roomNameHudStep = new UndertaleCode() { Name = gmData.Strings.MakeString("gml_Object_oRoomNameHUD_Step_0") };
+        SubstituteGMLCode(roomNameHudStep, """
+        if (displayMode == 0)
+            exit;
+        offsetY = 0
+        if instance_exists(oIGT)
+            offsetY = 11
+        if global.ingame
+        {
+            if (!fadeout)
+            {
+                if (image_alpha < 1)
+                    image_alpha += 0.2
+                else
+                    fadeout = 1
+            }
+            else if (displayMode == 1 && roomtime > 200 && image_alpha > 0)
+                image_alpha -= 0.02
+            roomtime++
+        }
+        if (surface_exists(rnh_surface) && surface_get_width(rnh_surface) < (320 + oControl.widescreen_space))
+            surface_free(rnh_surface)
+        if (!surface_exists(rnh_surface))
+            rnh_surface = surface_create((320 + oControl.widescreen_space), 240)
+        if surface_exists(rnh_surface)
+        {
+            surface_set_target(rnh_surface)
+            draw_clear_alpha(c_black, 0)
+            draw_set_font(global.fontGUI2)
+            draw_set_halign(fa_left)
+            draw_set_alpha(1)
+            draw_set_color(c_white)
+            draw_cool_text(4, (16 + offsetY), textToDisplay, textBGColor, textColor0, textColor1, image_alpha)
+            surface_reset_target()
+        }
+        """);
+        gmData.Code.Add(roomNameHudCC);
+        gmData.Code.Add(roomNameHudDestroy);
+        gmData.Code.Add(roomNameHudDrawGui);
+        gmData.Code.Add(roomNameHudRoomStart);
+        gmData.Code.Add(roomNameHudStep);
+        // Create Object and add events
+        
+        var roomHudObject = new UndertaleGameObject() { Name = gmData.Strings.MakeString("oRoomNameHUD"), Depth = -9999999, Persistent = true};
+        gmData.GameObjects.Add(roomHudObject);
+        AppendGMLInCode(gmData.Code.ByName("gml_Script_load_character_vars"), "if (!instance_exists(oRoomNameHUD))\ninstance_create(0, 0, oRoomNameHUD)");
+        // Create
+        var roomHudCollisionList = roomHudObject.Events[0];
+        var roomHudAction = new UndertaleGameObject.EventAction();
+        roomHudAction.CodeId = roomNameHudCC;
+        var roomHudEvent = new UndertaleGameObject.Event();
+        roomHudEvent.EventSubtype = 0;
+        roomHudEvent.Actions.Add(roomHudAction);
+        roomHudCollisionList.Add(roomHudEvent);
+        // Destroy
+        roomHudCollisionList = roomHudObject.Events[1];
+        roomHudAction = new UndertaleGameObject.EventAction();
+        roomHudAction.CodeId = roomNameHudDestroy;
+        roomHudEvent = new UndertaleGameObject.Event();
+        roomHudEvent.EventSubtype = 0;
+        roomHudEvent.Actions.Add(roomHudAction);
+        roomHudCollisionList.Add(roomHudEvent);
+        // Step
+        roomHudCollisionList = roomHudObject.Events[3];
+        roomHudAction = new UndertaleGameObject.EventAction();
+        roomHudAction.CodeId = roomNameHudStep;
+        roomHudEvent = new UndertaleGameObject.Event();
+        roomHudEvent.EventSubtype = 0;
+        roomHudEvent.Actions.Add(roomHudAction);
+        roomHudCollisionList.Add(roomHudEvent);
+        // Room start
+        roomHudCollisionList = roomHudObject.Events[7];
+        roomHudAction = new UndertaleGameObject.EventAction();
+        roomHudAction.CodeId = roomNameHudRoomStart;
+        roomHudEvent = new UndertaleGameObject.Event();
+        roomHudEvent.EventSubtype = 4;
+        roomHudEvent.Actions.Add(roomHudAction);
+        roomHudCollisionList.Add(roomHudEvent);
+        // Draw GUI
+        roomHudCollisionList = roomHudObject.Events[8];
+        roomHudAction = new UndertaleGameObject.EventAction();
+        roomHudAction.CodeId = roomNameHudDrawGui;
+        roomHudEvent = new UndertaleGameObject.Event();
+        roomHudEvent.EventSubtype = 64;
+        roomHudEvent.Actions.Add(roomHudAction);
+        roomHudCollisionList.Add(roomHudEvent);
+
         // Set fusion mode value
         ReplaceGMLInCode(gmData.Code.ByName("gml_Object_oControl_Step_0"), "mod_fusion = 0", $"mod_fusion = {(seedObject.Patches.FusionMode ? 1 : 0)}");
         
