d69bc232849c32125c5ac1c5e379007803283a8e
Miepee <38186597+Miepee@users.noreply.github.com>
2023-08-23 13:44:40 +0200

    Make missiles also not despawn when out of screen (#46)

--- YAMS-LIB/Program.cs
+++ YAMS-LIB/Program.cs
@@ -667,6 +667,11 @@ public class Patcher
         ReplaceGMLInCode(gmData.Code.ByName("gml_Object_oBeam_Step_0"), 
             "if (x < ((view_xview[0] - 48) - (oControl.widescreen_space / 2)) || x > (((view_xview[0] + view_wview[0]) + 48) + (oControl.widescreen_space / 2)) || y < (view_yview[0] - 48) || y > ((view_yview[0] + view_hview[0]) + 48))",
             "if (x > (room_width + 80) || x < -80 || y > (room_height + 80) || y < -160)");
+        
+        // Make Missiles not instantly despawn when out of screen
+        ReplaceGMLInCode(gmData.Code.ByName("gml_Object_oMissile_Step_0"),
+            "if (x < ((view_xview[0] - 48) - (oControl.widescreen_space / 2)) || x > (((view_xview[0] + view_wview[0]) + 48) + (oControl.widescreen_space / 2)) || y < (view_yview[0] - 48) || y > ((view_yview[0] + view_hview[0]) + 48))",
+            "if (x > (room_width + 80) || x < -80 || y > (room_height + 80) || y < -160)");
                 
         //No more Out of Bounds oSmallsplash crashes
         ReplaceGMLInCode(gmData.Code.ByName("gml_Object_oSmallSplash_Step_0"), "if (global.watertype == 0)", "if (global.watertype == 0 && instance_exists(oWater))");
