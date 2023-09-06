5547a5aadff13d64a7ba11604e66c7513417ed08
Miepee <janbidler00@protonmail.com>
2023-09-07 00:23:44 +0200

    Fix drill event from right side

--- YAMS-LIB/Program.cs
+++ YAMS-LIB/Program.cs
@@ -2796,7 +2796,10 @@ public class Patcher
             
         // When going down from thoth, make PB blocks disabled
         PrependGMLInCode(gmData.Code.ByName("gml_Room_rm_a0h13_Create"), "if (global.targety == 16) {global.event[176] = 1; with (oBlockPBombChain) event_user(0); }");
-            
+        
+        // When coming from right side in Drill, always make drill event done
+        PrependGMLInCode(gmData.Code.ByName("gml_Room_rm_a0h17e_Create"), "if (global.targety == 160) global.event[172] = 3");
+        
         // Stop Bomb blocks from respawning
         if (seedObject.Patches.RespawnBombBlocks)
             ReplaceGMLInCode(characterVarsCode, "global.respawnBombBlocks = 0", "global.respawnBombBlocks = 1");
