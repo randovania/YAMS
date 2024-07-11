910a30beb48716b5ae2bc049a626ca827a5aefdb
Miepee <janbidler00@protonmail.com>
2024-07-11 23:41:48 +0200

    Move room name dict into oControl

--- YAMS-LIB/patches/qol/DisplayRoomNamesOnMap.cs
+++ YAMS-LIB/patches/qol/DisplayRoomNamesOnMap.cs
@@ -20,7 +20,7 @@ public class DisplayRoomNamesOnMap
         string DSMapCoordRoomname = dsMapCordBuilder.ToString();
 
 
-        gmData.Code.ByName("gml_Object_oSS_Fg_Create_0").AppendGMLInCode(DSMapCoordRoomname);
+        gmData.Code.ByName("gml_Object_oControl_Create_0").PrependGMLInCode(DSMapCoordRoomname);
 
         gmData.Code.ByName("gml_Object_oSS_Fg_Draw_0").ReplaceGMLInCode("""
             draw_text((view_xview[0] + 161), (view_yview[0] + 30 - rectoffset), maptext)
@@ -31,7 +31,7 @@ public class DisplayRoomNamesOnMap
             var titleText = maptext;
             if (instance_exists(oMapCursor) && oMapCursor.active && oMapCursor.state == 1)
             {
-                titleText = ds_map_find_value(room_names_coords, string(global.mapmarkerx) + "|" + string(global.mapmarkery));
+                titleText = ds_map_find_value(oControl.room_names_coords, string(global.mapmarkerx) + "|" + string(global.mapmarkery));
                 if (titleText == undefined)
                     titleText = maptext;
             }
@@ -42,7 +42,7 @@ public class DisplayRoomNamesOnMap
         """);
 
         var ssFGDestroyCode = gmData.GameObjects.ByName("oSS_Fg").EventHandlerFor(EventType.Destroy, gmData);
-        ssFGDestroyCode.SubstituteGMLCode( "ds_map_destroy(room_names_coords)");
+        ssFGDestroyCode.SubstituteGMLCode("ds_map_destroy(oControl.room_names_coords)");
 
         // Adjust pause screen text to mention room names
         gmData.Code.ByName("gml_Object_oSS_Fg_Create_0").ReplaceGMLInCode( "tip2text = get_text(\"Subscreen\", \"Marker_Tip\")", "tip2text = \"| - Marker & Room Names\"");
