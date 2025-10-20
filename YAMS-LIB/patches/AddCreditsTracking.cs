7eb800160ed34c0d478396afc82f10ff480c7c61
Miepee <janbidler00@protonmail.com>
2025-10-20 21:52:06 +0200

    Fix patching

--- YAMS-LIB/patches/AddCreditsTracking.cs
+++ YAMS-LIB/patches/AddCreditsTracking.cs
@@ -9,6 +9,6 @@ public class AddCreditsTracking
     public static void Apply(UndertaleData gmData, GlobalDecompileContext decompileContext, SeedObject seedObject)
     {
         // Send credits item if ship is about to fly off.
-        gmData.Code.ByName("gml_Script_characterStepEvent").ReplaceGMLInCode("instance_create(3296, 1088, oShipOutro);", "global.collectedItems += \"Credits|1,\"; send_location_and_inventory_packet(); instance_create(3296, 1088, oShipOutro);");
+        gmData.Code.ByName("gml_Script_characterStepEvent").ReplaceGMLInCode("instance_create(3296, 1088, oShipOutro)", "global.collectedItems += \"Credits|1,\"; send_location_and_inventory_packet(); instance_create(3296, 1088, oShipOutro);");
     }
 }
